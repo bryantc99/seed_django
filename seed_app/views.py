96f7780c421206e56a0849ce2e3e4b0846ee3565
bryantc99 <bachen@stanford.edu>
2017-02-15 05:33:18 -0800

    see seed

--- seed_app/views.py
+++ seed_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.base import View
+from django.views.generic import TemplateView
 
 from django.http import HttpResponse
 
@@ -7,18 +7,7 @@ from django.http import HttpResponse
 import textwrap
 
 # Create your views here.
-class HomePageView(View):
 
-    def dispatch(request, *args, **kwargs):
-        response_text = textwrap.dedent('''\
-            <html>
-            <head>
-                <title>Greetings to the world</title>
-            </head>
-            <body>
-                <h1>Greetings to the world</h1>
-                <p>Hello, world!</p>
-            </body>
-            </html>
-        ''')
-        return HttpResponse(response_text)
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
