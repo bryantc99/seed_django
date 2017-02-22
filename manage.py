7160446c62c5822221adb36f67fa28f44b19c65e
bryantc99 <bachen@stanford.edu>
2017-02-21 16:09:30 -0800

    static root

--- manage.py
+++ manage.py
@@ -7,4 +7,7 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
+    PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
     execute_from_command_line(sys.argv)
